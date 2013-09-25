59b9b34c81d69982d73745efb499c9591da80908
Felix Dietze <felix@beerleader.net>
2013-09-25 20:36:34 +0200

    adjust time calculation

--- competition-organisation.scala
+++ competition-organisation.scala
@@ -1,13 +1,25 @@
 import scala.concurrent.duration._
 
-case class Round(competitorCount:Int, maxGroupSize:Int, winnerCount:Int) {
-  require( winnerCount <= maxGroupSize/2 )
-  require( competitorCount >= winnerCount )
+val competitorCount      = 70
+
+val lengthOfRun          = Duration( 2, MINUTES)
+val timePerRun           = Duration( 1, MINUTES)
+val timePerGroup         = Duration(15, MINUTES)
+val timePerRound         = Duration(20, MINUTES)
+val timePerCompetition   = Duration(20, MINUTES)
+
+val maxGroupSize         = 10
+val advancingCompetitors = 3
+
+case class Round(competitorCount:Int) {
+  require( advancingCompetitors <= maxGroupSize/2 )
+  require( competitorCount >= advancingCompetitors, s"Not enough competitors. Need at least $advancingCompetitors." )
   
-  def divFloor(a:Int, b:Int) = a / b
-  def divCeil(a:Int, b:Int) = if(a % b == 0) a / b else (a + ( b - (a % b) )) / b
+  private def divFloor(a:Int, b:Int) = a / b
+  private def divCeil(a:Int, b:Int) = if(a % b == 0) a / b else (a + ( b - (a % b) )) / b
   
   def groupCount = divCeil(competitorCount, maxGroupSize)
+  def runCount = competitorCount
   
   def freePlaces = maxGroupSize - (competitorCount % maxGroupSize)
   def smallGroupCount = freePlaces % groupCount
@@ -23,56 +35,46 @@ case class Round(competitorCount:Int, maxGroupSize:Int, winnerCount:Int) {
   assert( if( groupCount > 1 ) smallGroupSize >= maxGroupSize / 2 else true,  this)
   
   def next = {
-    require(groupSizes.forall( _ >= winnerCount))
-    Round(groupCount * winnerCount, maxGroupSize, winnerCount)
+    require(groupSizes.forall( _ >= advancingCompetitors))
+    Round(groupCount * advancingCompetitors)
   }
   
-  def roundCount:Int = 1 + (if( groupCount > 1 ) next.roundCount else 0)
-  def routineCount:Int = competitorCount + (if( groupCount > 1 ) next.routineCount else 0)
+  def totalRoundCount:Int = 1 + (if( groupCount > 1 ) next.totalRoundCount else 0)
+  def totalRunCount:Int = competitorCount + (if( groupCount > 1 ) next.totalRunCount else 0)
+  def totalGroupCount:Int = groupCount + (if( groupCount > 1 ) next.totalGroupCount else 0)
+  
+  def totalTimeNeeded = timePerCompetition +
+                   timePerRound * totalRoundCount +
+                   timePerGroup * totalGroupCount +
+                   (timePerRun + lengthOfRun) * totalRunCount
+  def timeNeeded = timePerRound +
+                   timePerGroup * groupCount +
+                   (timePerRun + lengthOfRun) * runCount
 }
 
 def occurrences(list:Iterable[_]) = list.groupBy(identity).mapValues(_.size)
+def prettyDuration(d:Duration) = (if(d.toHours > 0) s"${d.toHours}h " else "") + s"${(d - Duration(d.toHours, HOURS)).toMinutes}min"
 
-// tests for the assertions
-/*for( n <- 1 until 100;
-     m <- 1 until 100;
-     w <- 1 until 10;
-     if(w < m/2);
-     if(n >= w)
-   ) {
-  val a = Round(n,m,w)
-}*/
+var round = Round(competitorCount)
 
-
-var c = Round(competitorCount = 73, maxGroupSize = 10, winnerCount = 3)
-val routineDuration = Duration(3, MINUTES)
-val timeBetweenRounds = Duration(5, MINUTES)
 
 println("X-Style competition\n")
-println("configuration:")
-println(s"  max group size:      ${c.maxGroupSize}")
-println(s"  winners per group:   ${c.winnerCount}")
-println(s"  time per routine:    ${routineDuration}")
-println(s"  time between rounds: ${timeBetweenRounds}")
-println(s"  competitors:         ${c.competitorCount}")
-println()
 
-println("total:")
-println(s"  competitors:         ${c.competitorCount}")
-println(s"  number of routines:  ${c.routineCount}")
-println(s"  time needed:         ${routineDuration * c.routineCount + timeBetweenRounds * c.roundCount}")
+println(s"competitors:           ${round.competitorCount}")
+println(s"total number of runs:  ${round.totalRunCount}")
+println(s"total time needed:     ${prettyDuration(round.totalTimeNeeded)}")
 
 
 println()
 var i = 1
-while( c.competitorCount > 3 ) {
+while( round.competitorCount > advancingCompetitors ) {
   println(s"round $i")  
-  println(s"  competitors: ${c.competitorCount}")
-  println(s"  groups:      ${c.groupCount}")
-  println(s"  groupSizes:  ${occurrences(c.groupSizes).map{ case (number, count) => s"${count}x${number} riders"}.mkString(", ")}")
-  println(s"  time:        ${routineDuration * c.competitorCount + timeBetweenRounds}")
-  println(s"  the best ${c.winnerCount} of each group continue.")
+  println(s"  competitors: ${round.competitorCount}")
+  println(s"  groups:      ${round.groupCount}")
+  println(s"  groupSizes:  ${occurrences(round.groupSizes).map{ case (number, count) => s"${count}x${number} riders"}.mkString(", ")}")
+  println(s"  time:        ${prettyDuration(round.timeNeeded)}")
   println()
-  c = c.next
+  
+  round = round.next
   i += 1
 }
