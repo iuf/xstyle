a243e0443dbda4c76bb0b535b89c32e57c7a7016
Felix Dietze <felix@beerleader.net>
2014-05-08 20:46:13 +0200

    simplify tests

--- competition-software/src/test/scala/xstyle/libxstyle.scala
+++ competition-software/src/test/scala/xstyle/libxstyle.scala
@@ -4,60 +4,54 @@ import org.scalatest.FunSuite
 
 class LibXstyle extends FunSuite {
 
+  val ra = Rider(1,"a")
+  val rb = Rider(2,"b")
+  val rc = Rider(3,"c")
+  val rd = Rider(4,"d")
+  val re = Rider(5,"e")
+    
+  val comp = Competition(riders = List(ra, rb, rc, rd, re))
+
   test("advancing") {
-    val comp = Competition(riders = List(
-      Rider(1,"a"),
-      Rider(2,"b"),
-      Rider(3,"comp"),
-      Rider(4,"d")
-    ))
-    val round = comp.toRound
-    val groups = round.randomStartingGroups
-    val advanced = groups.head.advancingRiders(
+    val group = StartingGroup(comp.riders)
+    val advanced = group.advancingRiders(
       judges = List("x"),
       judgingSheets=List(
         JudgingSheet("x",
           List(
-            Placement(comp.rider(2),2),
-            Placement(comp.rider(4),3),
-            Placement(comp.rider(1),4),
-            Placement(comp.rider(3),1)
+            Placement(ra,rank=2),
+            Placement(rb,rank=3),
+            Placement(rc,rank=4),
+            Placement(rd,rank=1),
+            Placement(re,rank=5)
     ))))
 
-    assert( advanced.toSet == Set(comp.rider(2), comp.rider(3), comp.rider(4)) )
+    assert( advanced.toSet == Set(rd, ra, rb) )
   }
 
   test("ties") {
-    val comp = Competition(List(
-      Rider(1,"a"),
-      Rider(2,"b"),
-      Rider(3,"comp"),
-      Rider(4,"d"),
-      Rider(5,"e")
-    ))
-    val r = comp.toRound
-    val groups = r.randomStartingGroups
-    val advanced = groups.head.advancingRiders(
+    val group = StartingGroup(comp.riders)
+    val advanced = group.advancingRiders(
       judges = List("x", "y"),
       List(
         JudgingSheet("x",
           List(
-            Placement(comp.rider(1),1),
-            Placement(comp.rider(2),2),
-            Placement(comp.rider(3),3),
-            Placement(comp.rider(4),4),
-            Placement(comp.rider(5),5)
+            Placement(ra,1),
+            Placement(rb,2),
+            Placement(rc,3),
+            Placement(rd,4),
+            Placement(re,5)
           )),
         JudgingSheet("y",
           List(
-            Placement(comp.rider(1),1),
-            Placement(comp.rider(2),2),
-            Placement(comp.rider(3),4),
-            Placement(comp.rider(4),3),
-            Placement(comp.rider(5),5)
+            Placement(ra,1),
+            Placement(rb,2),
+            Placement(rc,4),
+            Placement(rd,3),
+            Placement(re,5)
           ))
       ))
 
-    assert( advanced.toSet == Set(comp.rider(1), comp.rider(2), comp.rider(3), comp.rider(4)) )
+    assert( advanced.toSet == Set(ra, rb, rc, rd) )
   }
 }
